20eee7d952b458fcb16f8a90fdfdddfdf83b7067
Yanarkaev <hasssanbek@gmail.com>
2023-09-19 23:55:46 +0300

    начал настройку поддержки css-модулей

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import "./Counter.scss"
+import style from "./Counter.module.scss"
 
 export const Counter = () => {
     const [count, setCount] = useState(0)
     return (
         <div>
-            <button onClick={() => setCount(prev => prev+=1)}>+</button>
+            <button className={style.inc} onClick={() => setCount(prev => prev+=1)}>+</button>
             <b>{count}</b>
             <button onClick={() => setCount(prev => prev-=1)}>-</button>
 
