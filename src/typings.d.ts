bddf10b36e81c3854f3ca357472d90957eeb8fcb
Yanarkaev <hasssanbek@gmail.com>
2023-09-20 23:34:22 +0300

    добавил поддержку css-modules

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,8 +1,7 @@
-declare module "*scss" {
+declare module '*.scss' {
   interface IClassNames {
-    [className: string]: string;
+    [className: string]: string
   }
-
   const classNames: IClassNames;
   export = classNames;
 }
