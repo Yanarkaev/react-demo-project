194ead547b06eae527c7e8253edcf4ee31411f99
Yanarkaev <hasssanbek@gmail.com>
2023-09-21 23:33:05 +0300

    добавил поддержку роутинга

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,23 @@
-import { Counter } from './Counter';
-import "../index.scss"
+import { Counter } from "./Counter";
+import "../index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "../pages/MainPage/MainPage.async";
+import {Suspense} from "react"
+
 
 export const App = () => {
-    return (
-        <div className='app'>
-            <Counter />
-        </div>
-    );
+  return (
+    <div className="app">
+      <Link to={"/"}>main</Link>
+      <Link to={"/about"}>about</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
