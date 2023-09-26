5b3799d74305ad25b8fab620a4cb9651c19780d3
Yanarkaev <hasssanbek@gmail.com>
2023-09-26 23:27:34 +0300

    добавил возможность смены темы через context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,19 @@
-import { Counter } from "./Counter";
-import "../index.scss";
+import "../styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "../pages/MainPage/MainPage.async";
-import {Suspense} from "react"
-
+import { Suspense } from "react";
+import { useTheme } from "../theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={"/"}>main</Link>
       <Link to={"/about"}>about</Link>
+
+      <button onClick={toggleTheme}>switch theme</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/about" element={<AboutPageAsync />} />
